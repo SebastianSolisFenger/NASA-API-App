680adb3c44584e7200d0baad6576d3ea2874849c
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-05-12 17:32:13 -0300

    make everything work as it is supposed to

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import './style.css';
-import { countElements } from './comments';
+import "./style.css";
+import { countElements } from "./comments";
 
-const getLink = 'https://api.nasa.gov/planetary/apod?api_key=4SuCuNxM0J6w1FmjQokTcawsubomH7aV4ep60VgT&start_date=2021-03-16&end_date=2021-03-21';
+const getLink =
+  "https://api.nasa.gov/planetary/apod?api_key=yE5XwF3YBRu6RaMb2K328lXJabWCog5rzjaIR76N&start_date=2021-03-16&end_date=2021-03-21";
 const datesForPopup = [
-  '2021-03-16',
-  '2021-03-17',
-  '2021-03-18',
-  '2021-03-19',
-  '2021-03-20',
-  '2021-03-21',
+  "2021-03-16",
+  "2021-03-17",
+  "2021-03-18",
+  "2021-03-19",
+  "2021-03-20",
+  "2021-03-21",
 ];
-const getImage = (num) => `https://api.nasa.gov/planetary/apod?api_key=4SuCuNxM0J6w1FmjQokTcawsubomH7aV4ep60VgT&date=${datesForPopup[num]}`;
-const main = document.getElementById('addToScreen');
-const starLink = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/9mAPgvMc6PjOJk4JU1ZU/likes';
-const commentLink = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/9mAPgvMc6PjOJk4JU1ZU/comments';
+const getImage = (num) =>
+  `https://api.nasa.gov/planetary/apod?api_key=yE5XwF3YBRu6RaMb2K328lXJabWCog5rzjaIR76N&date=${datesForPopup[num]}`;
+const main = document.getElementById("addToScreen");
+const starLink =
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/rS93TYMaWFRcDHR1Rs9u/likes";
+const commentLink =
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/rS93TYMaWFRcDHR1Rs9u/comments";
 
 const getScores = async (url) => {
   const response = await fetch(url);
@@ -22,9 +26,9 @@ const getScores = async (url) => {
 
 const postScores = async (url, data) => {
   const response = await fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   });
@@ -32,8 +36,8 @@ const postScores = async (url, data) => {
 };
 
 function addToScoreBord(img, title, index) {
-  const div = document.createElement('div');
-  div.classList.add('cardContainer');
+  const div = document.createElement("div");
+  div.classList.add("cardContainer");
   div.innerHTML = `
         <div class="imgcontainer">
           <img src="${img}" alt="">
@@ -53,8 +57,8 @@ function closePopup(target) {
 }
 
 function displayPopup(img, title, description, id) {
-  const popupDiv = document.createElement('div');
-  popupDiv.classList.add('popupWindow');
+  const popupDiv = document.createElement("div");
+  popupDiv.classList.add("popupWindow");
   popupDiv.innerHTML = `
   <div class="popupContainer">
    <span><i class="fas fa-times" id="close"></i>
@@ -80,31 +84,33 @@ function displayPopup(img, title, description, id) {
 }
 
 function countItems() {
-  const itemCount = document.querySelector('#item-count');
-  const section = document.querySelector('#addToScreen');
+  const itemCount = document.querySelector("#item-count");
+  const section = document.querySelector("#addToScreen");
   itemCount.firstChild.innerHTML = `APOD ${countElements(section)}`;
 }
 
 function countComments() {
-  const commentCount = document.querySelector('#comment-link');
+  const commentCount = document.querySelector("#comment-link");
   commentCount.previousElementSibling.innerHTML = `Comments ${countElements(
-    commentCount,
+    commentCount
   )}`;
 }
 
 function showComment(user, str) {
-  const ulCont = document.querySelector('#comment-link');
-  const li = document.createElement('li');
+  const ulCont = document.querySelector("#comment-link");
+  const li = document.createElement("li");
   li.innerHTML = `${user} : ${str}`;
   ulCont.appendChild(li);
 }
 
 function displayComments(id) {
-  const showProper = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/9mAPgvMc6PjOJk4JU1ZU/comments?item_id=${id}`;
+  const showProper = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/rS93TYMaWFRcDHR1Rs9u/comments?item_id=${id}`;
   getScores(showProper)
-    .then((data) => data.forEach((elem) => showComment(elem.username, elem.comment)))
+    .then((data) =>
+      data.forEach((elem) => showComment(elem.username, elem.comment))
+    )
     .then(() => countComments())
-    .catch(() => showComment('no', 'comments yet'));
+    .catch(() => showComment("no", "comments yet"));
 }
 
 function addComment(id, user, str) {
@@ -119,7 +125,7 @@ function addComment(id, user, str) {
         showComment(user, str);
       }
     })
-    .catch(() => showComment('no', 'comments yet'));
+    .catch(() => showComment("no", "comments yet"));
 }
 
 function displayImage(id) {
@@ -127,8 +133,8 @@ function displayImage(id) {
     .then((data) => displayPopup(data.hdurl, data.title, data.explanation, id))
     .then(() => {
       displayComments(id);
-      const closeBtn = document.getElementById('close');
-      closeBtn.addEventListener('click', () => {
+      const closeBtn = document.getElementById("close");
+      closeBtn.addEventListener("click", () => {
         closePopup(closeBtn);
       });
     })
@@ -142,17 +148,23 @@ const splitStars = (id, stars) => {
 
 function displayStars() {
   getScores(starLink)
-    .then((data) => data.forEach((elem, i) => {
-      if (i < countElements(main)) {
-        splitStars(elem.item_id, elem.likes);
-      }
-    }))
+    .then((data) =>
+      data.forEach((elem, i) => {
+        if (i < countElements(main)) {
+          splitStars(elem.item_id, elem.likes);
+        }
+      })
+    )
     .catch((err) => console.log(err));
 }
 
 function displayScores() {
   getScores(getLink)
-    .then((data) => data.forEach((elem, index) => addToScoreBord(elem.hdurl, elem.title, index)))
+    .then((data) =>
+      data.forEach((elem, index) =>
+        addToScoreBord(elem.hdurl, elem.title, index)
+      )
+    )
     .then(() => {
       displayStars();
       countItems();
@@ -173,26 +185,26 @@ function giveStar(id, stars) {
 
 displayScores();
 
-main.addEventListener('click', (e) => {
-  if (e.target.classList.contains('fa-star')) {
+main.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-star")) {
     e.preventDefault();
     const sC = parseInt(
       e.target.parentElement.parentElement.nextElementSibling.textContent,
-      10,
+      10
     );
     const stars = sC + 1;
     giveStar(e.target.parentElement.id, stars);
   }
-  if (e.target.classList.contains('comment')) {
+  if (e.target.classList.contains("comment")) {
     displayImage(parseInt(e.target.id, 10));
   }
-  if (e.target.id === 'popupComment') {
+  if (e.target.id === "popupComment") {
     e.preventDefault();
     const id = e.target.parentElement.id.match(/[0-9]/g);
-    const userName = document.getElementById('userName');
-    const comment = document.getElementById('insights');
+    const userName = document.getElementById("userName");
+    const comment = document.getElementById("insights");
     addComment(id, userName.value, comment.value);
-    userName.value = '';
-    comment.value = '';
+    userName.value = "";
+    comment.value = "";
   }
 });
